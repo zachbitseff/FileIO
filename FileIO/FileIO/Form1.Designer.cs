97b1683c5bea5ace528fba6703d8c42f2d1a4c3e
zachbitseff <38227293+zachbitseff@users.noreply.github.com>
2019-03-21 21:43:05 -0700

    Committed correct project files.

---
+++ FileIO/FileIO/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace FileIO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.readButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.txtDataContents = new System.Windows.Forms.TextBox();
+            this.encryptionButton = new System.Windows.Forms.Button();
+            this.decryptionButton = new System.Windows.Forms.Button();
+            this.simpleEncryptButton = new System.Windows.Forms.Button();
+            this.simpleDecryptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(86, 340);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(156, 62);
+            this.readButton.TabIndex = 0;
+            this.readButton.Text = "Read Text File";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(476, 340);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(163, 62);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save Text To File";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // txtDataContents
+            // 
+            this.txtDataContents.Location = new System.Drawing.Point(86, 46);
+            this.txtDataContents.Multiline = true;
+            this.txtDataContents.Name = "txtDataContents";
+            this.txtDataContents.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDataContents.Size = new System.Drawing.Size(553, 244);
+            this.txtDataContents.TabIndex = 2;
+            // 
+            // encryptionButton
+            // 
+            this.encryptionButton.Location = new System.Drawing.Point(651, 18);
+            this.encryptionButton.Name = "encryptionButton";
+            this.encryptionButton.Size = new System.Drawing.Size(137, 53);
+            this.encryptionButton.TabIndex = 3;
+            this.encryptionButton.Text = "Encrypt (Rijndael)";
+            this.encryptionButton.UseVisualStyleBackColor = true;
+            this.encryptionButton.Click += new System.EventHandler(this.encryptionButton_Click);
+            // 
+            // decryptionButton
+            // 
+            this.decryptionButton.Location = new System.Drawing.Point(651, 89);
+            this.decryptionButton.Name = "decryptionButton";
+            this.decryptionButton.Size = new System.Drawing.Size(137, 60);
+            this.decryptionButton.TabIndex = 4;
+            this.decryptionButton.Text = "Decrypt (Rijndael)";
+            this.decryptionButton.UseVisualStyleBackColor = true;
+            this.decryptionButton.Click += new System.EventHandler(this.decryptionButton_Click);
+            // 
+            // simpleEncryptButton
+            // 
+            this.simpleEncryptButton.Location = new System.Drawing.Point(665, 174);
+            this.simpleEncryptButton.Name = "simpleEncryptButton";
+            this.simpleEncryptButton.Size = new System.Drawing.Size(105, 41);
+            this.simpleEncryptButton.TabIndex = 5;
+            this.simpleEncryptButton.Text = "Encrypt";
+            this.simpleEncryptButton.UseVisualStyleBackColor = true;
+            this.simpleEncryptButton.Click += new System.EventHandler(this.simpleEncryptButton_Click);
+            // 
+            // simpleDecryptButton
+            // 
+            this.simpleDecryptButton.Location = new System.Drawing.Point(665, 221);
+            this.simpleDecryptButton.Name = "simpleDecryptButton";
+            this.simpleDecryptButton.Size = new System.Drawing.Size(105, 39);
+            this.simpleDecryptButton.TabIndex = 6;
+            this.simpleDecryptButton.Text = "Decrypt";
+            this.simpleDecryptButton.UseVisualStyleBackColor = true;
+            this.simpleDecryptButton.Click += new System.EventHandler(this.simpleDecryptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.simpleDecryptButton);
+            this.Controls.Add(this.simpleEncryptButton);
+            this.Controls.Add(this.decryptionButton);
+            this.Controls.Add(this.encryptionButton);
+            this.Controls.Add(this.txtDataContents);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File IO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox txtDataContents;
+        private System.Windows.Forms.Button encryptionButton;
+        private System.Windows.Forms.Button decryptionButton;
+        private System.Windows.Forms.Button simpleEncryptButton;
+        private System.Windows.Forms.Button simpleDecryptButton;
+    }
+}
+
---
+++ FileIO/FileIO/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace FileIO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string data = txtDataContents.Text;
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Contents |*.txt";
+            dialog.FileName = ".txt";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = dialog.FileName;
+                File.WriteAllText(fileName, data);
+                MessageBox.Show("Data saved");
+            } else
+            {
+                MessageBox.Show("Save error");
+            }
+
+            
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Contents|*.txt";
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = dialog.FileName;
+                txtDataContents.Text = File.ReadAllText(fileName);
+                isEncrypted = false;
+            }
+            encryptedSave = EncryptStringToBytes(txtDataContents.Text, myRijndael.Key, myRijndael.IV);
+        }
+
+        private byte[] encryptedSave;
+        private Rijndael myRijndael;
+        private Boolean isEncrypted = false;
+ 
+
+        private void encryptionButton_Click(object sender, EventArgs e)
+        {
+            if (!isEncrypted)
+            {
+                encryptedSave = EncryptStringToBytes(txtDataContents.Text, myRijndael.Key, myRijndael.IV);
+                isEncrypted = true;
+                string s = Convert.ToBase64String(encryptedSave);
+                txtDataContents.Text = s;
+            } else
+            {
+                MessageBox.Show("Data is already encrypted");
+            }
+        }
+
+        private void decryptionButton_Click(object sender, EventArgs e)
+        {
+
+            if (isEncrypted )
+            {
+                
+                txtDataContents.Text = DecryptStringFromBytes(encryptedSave, myRijndael.Key, myRijndael.IV);
+                
+                isEncrypted = false;
+            } else
+            {
+                MessageBox.Show("Data is already decrypted");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myRijndael = Rijndael.Create();
+        }
+
+   static byte[] EncryptStringToBytes(string plainText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+            // Create an Rijndael object
+            // with the specified key and IV.
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+                rijAlg.Padding = PaddingMode.PKCS7;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = rijAlg.CreateEncryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+
+                            //Write all data to the stream.
+                            swEncrypt.Write(plainText);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+
+        }
+
+        static string DecryptStringFromBytes(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string plaintext = null;
+
+            // Create an Rijndael object
+            // with the specified key and IV.
+            using (Rijndael rijAlg = Rijndael.Create())
+            {
+                rijAlg.Key = Key;
+                rijAlg.IV = IV;
+                rijAlg.Padding = PaddingMode.PKCS7;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = rijAlg.CreateDecryptor(rijAlg.Key, rijAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+
+            }
+
+            return plaintext;
+        
+        }
+
+        private Int16 shift = 3;
+
+        private void simpleEncryptButton_Click(object sender, EventArgs e)
+        {
+            txtDataContents.Text = Cipher(txtDataContents.Text, shift);
+        }
+
+        private void simpleDecryptButton_Click(object sender, EventArgs e)
+        {
+            txtDataContents.Text = Decipher(txtDataContents.Text, shift);
+        }
+
+        // credit https://www.youtube.com/watch?v=2ruqxHVlZfM
+        static string Cipher(string text, int shift)
+        {
+            var result = string.Empty; 
+
+            
+            var firstCharCode = 'A';
+            var offset = ('z' - 'A') + 1;
+
+            foreach (var c in text)
+            {
+                var newChar = ' ';
+
+                if ( c != ' ' )
+                {
+                    var oldIdxInAlphabet = c - firstCharCode;
+
+                    var idxShifted = oldIdxInAlphabet + shift;
+                    while (idxShifted < 0) idxShifted = offset + idxShifted;
+
+                    var newIdxInAlphabet = idxShifted % offset;
+
+                    newChar = (char)(firstCharCode + newIdxInAlphabet);
+                }
+
+                result += newChar;
+            }
+
+
+
+            return result;
+        }
+
+        static string Decipher(string CipheredText, int shift)
+        {
+            return Cipher(CipheredText, shift * -1);
+        }
+    }
+}
